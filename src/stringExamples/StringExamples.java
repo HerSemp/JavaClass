d67a4e01b0ae8d4a67210891691cac1e83ac69d9
Her <hersemp@gmail.com>
2022-09-23 11:17:33 +0400

    Exercise

--- src/stringExamples/StringExamples.java
+++ src/stringExamples/StringExamples.java
@@ -6,6 +6,15 @@ public class StringExamples {
         String s = "Hello";
         System.out.println(s+" Esan");
 
+
+        /**
+        try{
+            s.append("Hello");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+         */
+
         StringBuffer b = new StringBuffer();
         b.append("Hello ");
         b.append(s);
